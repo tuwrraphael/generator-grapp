d659328916f604dcfe2a48c1b8a0c5dd98b318e0
Raphael <raphael.hauk@racing.tuwien.ac.at>
2021-03-30 18:48:21 +0200

    fix dotfiles

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,6 @@
 "use strict";
 const Generator = require("yeoman-generator");
+const dashify = require("dashify");
 
 module.exports = class extends Generator {
   async prompting() {
@@ -20,7 +21,17 @@ module.exports = class extends Generator {
   writing() {
     this.fs.copyTpl(this.templatePath("**/*"), this.destinationRoot(), {
       appname: this.answers.appname,
-      appdescription: this.answers.appdescription
+      appdescription: this.answers.appdescription,
+      packagename: dashify(this.answers.appname)
     });
+    this.fs.copy(this.templatePath(".*"), this.destinationRoot());
+    this.fs.copy(
+      this.templatePath(".github/**/*"),
+      this.destinationRoot() + "/.github"
+    );
+    this.fs.copy(
+      this.templatePath(".vscode/**/*"),
+      this.destinationRoot() + "/.vscode"
+    );
   }
 };
